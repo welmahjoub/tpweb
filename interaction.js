8308a98b277777d196a3d5180d2317a9c681bfbc
El Mahjoub <mahjoubwel@gmail.com>
2020-01-23 09:34:33 +0100

    partie 1 ok

--- interaction.js
+++ interaction.js
@@ -4,9 +4,53 @@
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
 
-	// Developper les 3 fonctions gérant les événements
+	//poistion initiale
+	this.x1=0;
+	this.y1=0;
 
+	// position final
+	this.x2=0;
+	this.y2=0;
+
+    this.press=false;
+
+	// Developper les 3 fonctions gérant les événements
+     
+     function pression(event)
+     {
+
+     	this.press=true;	
+
+     }.bind(this); // lies la method a la classe Dnd
+
+     function deplacer(event)
+     {
+     	if(press)
+     	{
+     		var res=getMousePosition(canvas,event);
+     		this.x2=res.x;
+     		this.y2=res.y;
+     	}
+
+     }.bind(this); // lies la method a la classe Dnd
+
+     function relacher(event)
+     {
+     	if(press)
+     	{
+     		var res=getMousePosition(canvas,event);
+     		this.x2=res.x;
+     		this.y2=res.y;
+     	}
+
+     }.bind(this); // lies la method a la classe Dnd
+	
 	// Associer les fonctions précédentes aux évènements du canvas.
+
+	canvas.addEventListener('mousedown', this.pression, false);
+	canvas.addEventListener('mousemove', this.deplacer, false);
+	canvas.addEventListener('mouseup', this.relacher, false);
+
 };
 
 
